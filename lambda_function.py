2b1c71a641933283e0b70c52e20c0449478aa1f0
abdulrawoof <aiyubalikhan92@gmail.com>
2023-04-13 16:21:49 +0530

    4

--- lambda_function.py
+++ lambda_function.py
@@ -8,13 +8,15 @@
 #     }
 
 import boto3
-aws_mag_con=boto3.session.Session(aws_access_key_id="AKIARIQWLXYMXHSQ3A4L",aws_secret_access_key="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")
-ec2_con_re=aws_mag_con.resource(service_name="ec2",region_name="us-east-1")
-ec2_con_cli=aws_mag_con.client(service_name="ec2",region_name="us-east-1")
 
-f1={"Name": "instance-state-name", "Values":['running','pending']}
+def lambda_handler(event, context):
+    aws_mag_con=boto3.session.Session(aws_access_key_id="AKIARIQWLXYMXHSQ3A4L",aws_secret_access_key="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")
+    ec2_con_re=aws_mag_con.resource(service_name="ec2",region_name="us-east-1")
+    ec2_con_cli=aws_mag_con.client(service_name="ec2",region_name="us-east-1")
 
-f2={"Name": "instance-type", "Values":['t2.micro']}
+    f1={"Name": "instance-state-name", "Values":['running','pending']}
 
-for each in ec2_con_re.instances.filter(Filters=[f1,f2]):
-    print(each)
+    f2={"Name": "instance-type", "Values":['t2.micro']}
+
+    for each in ec2_con_re.instances.filter(Filters=[f1,f2]):
+        print(each)
