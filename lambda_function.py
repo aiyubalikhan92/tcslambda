4efa8d960671a128d1a2afb98e682881608482c9
aiyubalikhan92 <aiyubalikhan92@gmail.com>
2023-04-23 19:03:12 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('An Mean nasib aiyubn!')
+        'body': json.dumps('An Mean nasib tcs demo!')
     }
 
 # import boto3
