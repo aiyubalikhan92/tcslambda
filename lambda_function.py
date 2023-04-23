67a38315a60e82479acaab4c21b93e7e9ef3bf94
aiyubalikhan92 <aiyubalikhan92@gmail.com>
2023-04-23 08:57:51 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -7,18 +7,32 @@
 #         'body': json.dumps('An Mean nasib!')
 #     }
 
-import boto3
+# import boto3
 
-def lambda_handler(event, context):
-    aws_mag_con=boto3.session.Session(aws_access_key_id="AKIARIQWLXYMXHSQ3A4L",aws_secret_access_key="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")
-    ec2_con_re=aws_mag_con.resource(service_name="ec2",region_name="us-east-1")
-    ec2_con_cli=aws_mag_con.client(service_name="ec2",region_name="us-east-1")
+# def lambda_handler(event, context):
+#     aws_mag_con=boto3.session.Session(aws_access_key_id="AKIARIQWLXYMXHSQ3A4L",aws_secret_access_key="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")
+#     ec2_con_re=aws_mag_con.resource(service_name="ec2",region_name="us-east-1")
+#     ec2_con_cli=aws_mag_con.client(service_name="ec2",region_name="us-east-1")
 
-    f1={"Name": "instance-state-name", "Values":['running','pending']}
+#     f1={"Name": "instance-state-name", "Values":['running','pending']}
 
-    f2={"Name": "instance-type", "Values":['t2.micro']}
+#     f2={"Name": "instance-type", "Values":['t2.micro']}
 
-    for each in ec2_con_re.instances.filter(Filters=[f1,f2]):
-        print(each)
+#     for each in ec2_con_re.instances.filter(Filters=[f1,f2]):
+#         print(each)
 
-lambda_handler('test','demo')
+# lambda_handler('test','demo')
+
+import numpy
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+arrr = numpy.array([1, 2, 3, 4, 5])
+
+print(arrr)
+
+print(arr)
+
+print(type(arr))
