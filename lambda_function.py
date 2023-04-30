33b3af46dfea37cd6fe3d032070a7fdcccfd3c05
aiyubalikhan92 <aiyubalikhan92@gmail.com>
2023-04-30 22:02:35 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,10 +5,10 @@ import sys
 #print(os.environ['date'])
 
 def lambda_handler(event, context):
-    print(event['date'])
+    #print(event['date'])
     #thisInstanceID = event['date']
     #print("date: " + thisInstanceID)
-    #os.environ.get("date")
+    os.environ.get("date")
     #os.environ.get('BUILD_ID')
     #qualifier = os.environ['QUALIFIER']
     #print(os.environ['qualifier'])
